9fdfbc2d769aae87f664bf197774602b45041352
Stefan <stefan.anton@mkodo.com>
2020-05-03 20:09:41 +0100

    Clean up

--- tennis-calc.js
+++ tennis-calc.js
@@ -1,26 +1,29 @@
 function score(player1Points, player2Points) {
-    const displayScore = ["Love", "Fifteen", "Thirty", "Forty"];
-    
-    if(player1Points === 3) {
-        return displayScore[player1Points]+"-Love";
+    if (player1Points === 3) {
+        return scoreFor(player1Points) + "-Love";
     }
-    if(player1Points === 2) {
-        return displayScore[player1Points]+"-Love";
+    if (player1Points === 2) {
+        return scoreFor(player1Points) + "-Love";
     }
-    if(player1Points === 1) {
-        return displayScore[player1Points]+"-Love";
+    if (player1Points === 1) {
+        return scoreFor(player1Points) + "-Love";
     }
     
-    if(player2Points === 3) {
+    if (player2Points === 3) {
         return "Love-Forty";
     }
-    if(player2Points === 2) {
+    if (player2Points === 2) {
         return "Love-Thirty";
     }
-    if(player2Points === 1) {
+    if (player2Points === 1) {
         return "Love-Fifteen";
     }
     return "Love-Love";
+}
+
+function scoreFor(points) {
+    const displayScore = ["Love", "Fifteen", "Thirty", "Forty"];
+    return displayScore[points];
 }
 
 module.exports = score;
