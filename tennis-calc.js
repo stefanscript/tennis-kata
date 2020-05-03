a956f25e95f7be8baf454a0b5bcc4a61bf90727d
Stefan <stefan.anton@mkodo.com>
2020-05-03 20:49:23 +0100

    we win

--- tennis-calc.js
+++ tennis-calc.js
@@ -1,9 +1,6 @@
 function score(player1Points, player2Points) {
     if ((player1Points > 3 || player2Points > 3) && Math.abs(player1Points - player2Points) === 2) {
-        if (player2Points > 3) {
-            return "Player 2 Wins";
-        }
-        return "Player 1 Wins";
+        return "Player " + (player2Points > 3 ?  "2" : "1") + " Wins";
     }
     
     if (player1Points >= 3 && player2Points >= 3) {
