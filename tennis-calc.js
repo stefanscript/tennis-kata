82b672fe31a42d77c8c775a317771f0ada69ac7a
Stefan <stefan.anton@mkodo.com>
2020-05-03 19:53:08 +0100

    Woo we can score one point

--- tennis-calc.js
+++ tennis-calc.js
@@ -1,4 +1,7 @@
 function score(player1Points, player2Points) {
+    if(player1Points === 1) {
+        return "Fifteen-Love";
+    }
     return "Love-Love";
 }
 
--- tennis-calc.spec.js
+++ tennis-calc.spec.js
@@ -9,5 +9,14 @@ describe("Tennis calculator", () =>  {
         
         expect(result).toEqual("Love-Love");
     });
+    
+    it("should return fifteen love when one player scores", () => {
+        const player1Points = 1;
+        const player2Points = 0;
+        
+        const result = score(player1Points, player2Points);
+        
+        expect(result).toEqual("Fifteen-Love");
+    });
 });
 
