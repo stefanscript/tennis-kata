fe7168ff84d8b8829e31a03c33663219ce116d8b
Stefan <stefan.anton@mkodo.com>
2020-05-03 20:07:55 +0100

    refactor score method to use a map

--- tennis-calc.js
+++ tennis-calc.js
@@ -1,12 +1,14 @@
 function score(player1Points, player2Points) {
+    const displayScore = ["Love", "Fifteen", "Thirty", "Forty"];
+    
     if(player1Points === 3) {
-        return "Forty-Love";
+        return displayScore[player1Points]+"-Love";
     }
     if(player1Points === 2) {
-        return "Thirty-Love";
+        return displayScore[player1Points]+"-Love";
     }
     if(player1Points === 1) {
-        return "Fifteen-Love";
+        return displayScore[player1Points]+"-Love";
     }
     
     if(player2Points === 3) {
