6307aee64806b3051bc8b2a785eac4b82b09f45e
Stefan <stefan.anton@mkodo.com>
2020-05-03 20:20:35 +0100

    Further clean up, may regret it in a bit

--- tennis-calc.js
+++ tennis-calc.js
@@ -1,30 +1,11 @@
 function score(player1Points, player2Points) {
     const score1 = convert(player1Points);
-    if (score1 !== undefined) {
-        return score1 + "-Love";
-    }
-    
     const score2 = convert(player2Points);
-    if (score2 !== undefined) {
-        return "Love-" + score2;
-    }
     
-    return "Love-Love";
+    return `${score1}-${score2}`;
 }
 
 function convert(points) {
-    if (points === 3) {
-        return scoreFor(points);
-    }
-    if (points === 2) {
-        return scoreFor(points);
-    }
-    if (points === 1) {
-        return scoreFor(points);
-    }
-}
-
-function scoreFor(points) {
     return ["Love", "Fifteen", "Thirty", "Forty"][points];
 }
 
