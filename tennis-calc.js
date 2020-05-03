358cb3c570a20dea29f363dfd42658d8809ccc55
Stefan <stefan.anton@mkodo.com>
2020-05-03 21:01:40 +0100

    refactor to show the tennis score rules

--- tennis-calc.js
+++ tennis-calc.js
@@ -1,18 +1,29 @@
 function score(player1Points, player2Points) {
-    if ((player1Points > 3 || player2Points > 3) && Math.abs(player1Points - player2Points) === 2) {
-        return "Player " + (player2Points > 3 ?  "2" : "1") + " Wins";
+    if (isWin(player1Points, player2Points)) {
+        return "Player " + (player2Points > 3 ? "2" : "1") + " Wins";
     }
     
-    if (player1Points >= 3 && player2Points >= 3) {
-        if (player1Points !== player2Points) {
-            return "Advantage Player " + (player1Points > player2Points ? "1" : "2");
-        }
+    if (isAdvantage(player1Points, player2Points)) {
+        return "Advantage Player " + (player1Points > player2Points ? "1" : "2");
+    }
+    
+    if (isDeuce(player1Points, player2Points)) {
         return "Deuce";
     }
-    const score1 = convert(player1Points);
-    const score2 = convert(player2Points);
     
-    return `${score1}-${score2}`;
+    return `${convert(player1Points)}-${convert(player2Points)}`;
+}
+
+function isWin(player1Points, player2Points) {
+    return Math.max(player1Points, player2Points) > 3 && Math.abs(player1Points - player2Points) === 2;
+}
+
+function isAdvantage(player1Points, player2Points) {
+    return player1Points >= 3 && player2Points >= 3 && player1Points !== player2Points;
+}
+
+function isDeuce(player1Points, player2Points) {
+    return player1Points >= 3 && player2Points >= 3;
 }
 
 function convert(points) {
