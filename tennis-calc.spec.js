37785db70a336522a60f654f0179d5d0908f1619
Stefan <stefan.anton@mkodo.com>
2020-05-03 19:56:21 +0100

    Clean uop tests

--- tennis-calc.spec.js
+++ tennis-calc.spec.js
@@ -1,22 +1,13 @@
 const score = require("./tennis-calc");
 
 describe("Tennis calculator", () =>  {
-    it("should get a score", () => {
-        const player1Points = 0;
-        const player2Points = 0;
-        
-        const result = score(player1Points, player2Points);
-        
-        expect(result).toEqual("Love-Love");
-    });
-    
-    it("should return fifteen love when one player scores", () => {
-        const player1Points = 1;
-        const player2Points = 0;
-        
-        const result = score(player1Points, player2Points);
-        
-        expect(result).toEqual("Fifteen-Love");
+    it("should get a basic score", () => {
+        toTennisScore(0, 0, "Love-Love");
+        toTennisScore(1, 0, "Fifteen-Love");
     });
 });
+
+function toTennisScore(player1Points, player2Points, expected) {
+    expect(score(player1Points, player2Points)).toEqual(expected);
+}
 
