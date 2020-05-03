697a0d018efcf456bf56729e4938d44402b29723
Stefan <stefan.anton@mkodo.com>
2020-05-03 20:17:58 +0100

    extract a new function for player1 score

--- tennis-calc.js
+++ tennis-calc.js
@@ -1,12 +1,7 @@
 function score(player1Points, player2Points) {
-    if (player1Points === 3) {
-        return scoreFor(player1Points) + "-Love";
-    }
-    if (player1Points === 2) {
-        return scoreFor(player1Points) + "-Love";
-    }
-    if (player1Points === 1) {
-        return scoreFor(player1Points) + "-Love";
+    const score1= convert(player1Points);
+    if (score1 !== undefined) {
+        return score1 + "-Love";
     }
     
     if (player2Points === 3) {
@@ -19,6 +14,18 @@ function score(player1Points, player2Points) {
         return "Love-" + scoreFor(player2Points);
     }
     return "Love-Love";
+}
+
+function convert(points) {
+    if (points === 3) {
+        return scoreFor(points);
+    }
+    if (points === 2) {
+        return scoreFor(points);
+    }
+    if (points === 1) {
+        return scoreFor(points);
+    }
 }
 
 function scoreFor(points) {
