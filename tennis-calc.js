3a25ec8f7be86570c6a972e6f6a39e480371eabc
Stefan <stefan.anton@mkodo.com>
2020-05-03 21:10:19 +0100

    That's it. NO more. I'm getting a cuppa tea

--- tennis-calc.js
+++ tennis-calc.js
@@ -3,19 +3,23 @@ function score(player1Points, player2Points) {
         return "Player " + (player2Points > 3 ? "2" : "1") + " Wins";
     }
     
-    if (isAdvantage(player1Points, player2Points)) {
-        return "Advantage Player " + (player1Points > player2Points ? "1" : "2");
-    }
-    
     if (isDeuce(player1Points, player2Points)) {
         return "Deuce";
+    }
+    
+    if (isAdvantage(player1Points, player2Points)) {
+        return "Advantage Player " + (player1Points > player2Points ? "1" : "2");
     }
     
     return `${convert(player1Points)}-${convert(player2Points)}`;
 }
 
 function isWin(player1Points, player2Points) {
-    return Math.max(player1Points, player2Points) > 3 && Math.abs(player1Points - player2Points) === 2;
+    return Math.max(player1Points, player2Points) > 3 && isTwoPointDifference(player1Points, player2Points);
+}
+
+function isTwoPointDifference(player1Points, player2Points) {
+    return Math.abs(player1Points - player2Points) === 2;
 }
 
 function isAdvantage(player1Points, player2Points) {
@@ -23,7 +27,7 @@ function isAdvantage(player1Points, player2Points) {
 }
 
 function isDeuce(player1Points, player2Points) {
-    return player1Points >= 3 && player2Points >= 3;
+    return player1Points >= 3 && player2Points >= 3 && player1Points === player2Points;
 }
 
 function convert(points) {
